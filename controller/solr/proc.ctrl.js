5370f9b583e2d10c1df617dde8d6720875999535
Baekwangho <qorhkdgh00@gmail.com>
2021-11-05 23:51:00 +0900

    delete/modify module init

--- controller/solr/proc.ctrl.js
+++ controller/solr/proc.ctrl.js
@@ -1,15 +1,80 @@
 const { convertCrawlDocTo } = require("../../lib/libs");
-const solrDB = require("../../models/solr/index");
+const solrDB = require("../../models/solr/index").solrDB;
+const config = require("../../models/solr/index").config;
+const axios = require("axios");
+
+const procDelete = (req) => new Promise((resolve,reject)=>{
+    resolve(true);
+    /*
+    let itemId = req.params.itemId;
+    var query = 'item_id:'+itemId;
+
+    solrDB.deleteByQuery(query,function(err,obj){
+        if(err){
+            resolve(false);
+        }else{
+            resoleve(true);
+        }
+       // Do not forget to commit now
+       // to see the changes
+    });
+    */
+})
+
+const procKeep = (req) => new Promise(async (resolve,reject)=>{
+    resolve(true);
+    /*
+    //http://10.224.143.172:8983/solr/knowledge_combined/update?commit=true --data-binary $'[{"id" : "1", "site_name"  : {"set":"Test new change"} }]'
+    let query = {
+        'item_id': req.params.itemId,
+        'stat':{'set':1},
+        'language':{'set':["ja","en"]}
+    };
+
+    const data = JSON.stringify(query);
+    
+    await axios.get("http://"+config.host+":"+config.port+"/solr/"+config.core+"/update?commit=true --data-binary $'["+data+"]'")
+    .then((result)=>{
+        console.log("pass");
+        //console.log(result);
+    }).catch((err)=>{
+        console.log("no");
+
+        //console.log(err);
+    });
+    resolve(true);
+    */
+    
+    /*
+    let itemId = req.body.itemId;
+    let query = {
+        'item_id': itemId,
+        'stat':{'set':1},
+        'language':{'set':["ja","en"]}
+    };
+    
+    solrDB.add(query, function(err, obj) {
+        if (err) {
+            console.log(err);
+            resolve(false);
+        } else {
+            solrDB.softCommit();
+            //작성 요
+            resolve(true);
+        }
+      });
+      */
+});
 
 const procDetail = (req) => new Promise((resolve,reject)=> {
     let query = 'q=item_id:'+req.params.itemId;
     query = encodeURI(query);
     solrDB.search(query, function(err, obj){
         if(err){
-            reject();
+            
+            resolve(false);
         }else{
             const newDocs = [];
-            
             obj.response.dcCount = obj.response.numFound;
             obj.response.docs.forEach((document)=>{
                 newDocs.push(convertCrawlDocTo(document,'solr'));
@@ -87,7 +152,7 @@ const procSearch = (req) => new Promise(async (resolve,reject)=>{
     query = encodeURI(query);
     solrDB.search(query, function(err, obj){
         if(err){
-            reject();
+            resolve(false);
         }else{
             const newDocs = [];
             obj.response.dcCount = obj.response.numFound;
@@ -103,7 +168,9 @@ const procSearch = (req) => new Promise(async (resolve,reject)=>{
 
 module.exports = {
     Search:procSearch,
-    Detail:procDetail
+    Detail:procDetail,
+    Keep:procKeep,
+    Delete:procDelete
 };
 
 
