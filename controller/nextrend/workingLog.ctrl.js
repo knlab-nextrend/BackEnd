c7d5204634e9d6023786cbea2fafaf113ca9db91
BaeKwangho <qorhkdgh00@gmail.com>
2022-03-09 18:55:01 +0900

    screen 오탈자 수정

--- controller/nextrend/workingLog.ctrl.js
+++ controller/nextrend/workingLog.ctrl.js
@@ -17,7 +17,7 @@ const addEditLog = (wid, esId, docStat, workType ) => new Promise((resolve,rejec
 })
 
 const addScreeningLog = (wid, docStat, workType, quantity )=> new Promise((resolve,reject)=>{
-    const query = 'insert into nt_screening_log (wid, doc_stat, work_type, qunatity) values (?,?,?,?);'
+    const query = 'insert into nt_screen_log (wid, doc_stat, work_type, quantity) values (?,?,?,?);'
     const params = [wid,docStat,workType,quantity];
     db.query(query, params, (err,data) => {
         if(err){
@@ -30,7 +30,7 @@ const addScreeningLog = (wid, docStat, workType, quantity )=> new Promise((resol
 
 // 큐레이션의 경우 동작은.. 수정과 이관이 있을 것임. 하지만 정량 체크가 우선이기에 관련 정보는 nt_edit_log에서 관리.
 const addCurationLog = (wid, esId, quality, quantity)=> new Promise((resolve,reject)=>{
-    const query = 'insert into nt_curation_log (wid, es_id, quality, qunatity) values (?,?,?,?);'
+    const query = 'insert into nt_curation_log (wid, es_id, quality, quantity) values (?,?,?,?);'
     const params = [wid,esId,quality,quantity];
     db.query(query, params, (err,data) => {
         if(err){
