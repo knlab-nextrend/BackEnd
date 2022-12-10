2dacf9cc0c0f3b61f870205c40d290a9424c0dd4
brobac <issg2030@gmail.com>
2022-12-10 23:08:22 +0900

    feat. 사용자 아카이화면 수정 - 테이블 행 여백줄임 - 컬럼별 너비 조절 - 행 내용 줄바꿈 되지 않게 말줄임 처리

--- FrontEnd/src/Pages/User/UserOnlyDataLookUpPage/UserArchiveDataList.js
+++ FrontEnd/src/Pages/User/UserOnlyDataLookUpPage/UserArchiveDataList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { HiOutlineDocumentSearch } from "react-icons/hi";
-import Pagination from "../../../Components/Pagination";
-import NoData from "../../../Components/NoData";
+import Pagination from "Components/Pagination";
+import NoData from "Components/NoData";
+import { SearchResultCount } from "Components/SearchResultCount";
 import { myColors, tailwindColors } from "styles/colors";
 import { toYYMMDD_DOT } from "Utils/time";
+
 function UserArchiveDataList({
   dcCount,
   listSize,
@@ -16,11 +17,7 @@ function UserArchiveDataList({
 }) {
   return (
     <Wrap>
-      <ResultWrap>
-        <HiOutlineDocumentSearch />
-        <span> 검색 결과</span> <span>({dcCount}건)</span>
-      </ResultWrap>
-
+      <SearchResultCount count={dcCount} />
       {archiveData.length === 0 ? (
         <NoData />
       ) : (
@@ -35,13 +32,13 @@ function UserArchiveDataList({
           </ActionGroup>
           <ArchiveDataTable>
             <colgroup>
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "30%" }} />
-              <col style={{ width: "15%" }} />
-              <col style={{ width: "15%" }} />
-              <col style={{ width: "10%" }} />
-              <col style={{ width: "10%" }} />
+              <col />
+              <col />
+              <col />
+              <col />
+              <col />
+              <col />
+              <col />
             </colgroup>
             <thead>
               <tr>
@@ -51,34 +48,38 @@ function UserArchiveDataList({
                 <th>발행기관 명</th>
                 <th>원문 발행일</th>
                 <th>페이지 수</th>
-                <th>큐레이션 선정</th>
+                <th>큐레이션 신청</th>
               </tr>
             </thead>
             <tbody>
               {archiveData.map((item, index) => {
                 return (
                   <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{item.doc_country_list}</td>
-                    <td>
-                      <a href={item.doc_url} target="_blank">
+                    <td>{listSize * (pageNo - 1) + index + 1}</td>
+                    <td className="country-list">{item.doc_country_list}</td>
+                    <td className="origin-title">
+                      <a href={item.doc_url} rel="noreferrer" target="_blank">
                         {item.doc_origin_title}
                       </a>
                     </td>
-                    <td>{item.doc_publisher}</td>
-                    <td>{toYYMMDD_DOT(item.doc_publish_date)}</td>
-                    <td>{item.doc_page}</td>
-                    <td>
+                    <td className="publisher">{item.doc_publisher}</td>
+                    <td className="publish-date">
+                      {toYYMMDD_DOT(item.doc_publish_date)}
+                    </td>
+                    <td className="page">
+                      {item.doc_page && `${item.doc_page}p`}
+                    </td>
+                    <td className="curation-request">
                       {item.status === 6 ? (
                         <button
                           onClick={() => {
                             curationRequest(item._id);
                           }}
                         >
-                          큐레이션 신청
+                          신청하기
                         </button>
                       ) : (
-                        <Badge>큐레이션 신청됨</Badge>
+                        <div>신청완료</div>
                       )}
                     </td>
                   </tr>
@@ -109,79 +110,6 @@ const Wrap = styled.div`
   padding: 1.5rem 3rem;
 `;
 
-const ResultWrap = styled.div`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  padding: 1rem 0;
-  font-size: 1.5rem;
-  font-weight: bold;
-  white-space: nowrap;
-
-  & > span:nth-of-type(2) {
-    color: ${myColors.red};
-  }
-`;
-
-const ArchiveDataTable = styled.table`
-  width: 100%;
-  table-layout: fixed;
-  border-collapse: collapse;
-  text-align: center;
-  font-size: 14px;
-
-  thead {
-    border-bottom: 1px solid ${tailwindColors["grey-400"]};
-    background-color: ${tailwindColors["grey-100"]};
-    font-weight: bold;
-    text-align: center;
-    white-space: nowrap;
-  }
-
-  th,
-  td {
-    padding: 0.5rem;
-    word-break: break-all;
-  }
-
-  & tbody {
-    width: 100%;
-
-    tr {
-      width: 100%;
-      height: 2.5rem;
-      border-bottom: 1px solid ${tailwindColors["grey-400"]};
-      background-color: ${tailwindColors.white};
-    }
-  }
-
-  th,
-  td {
-    border-bottom: 1px solid #d6d6d6;
-    padding: 10px;
-    a {
-    }
-    a:hover {
-      color: ${myColors.blue500};
-      text-decoration: underline;
-    }
-  }
-
-  button {
-    height: 2rem;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: solid 1px rgba(0, 0, 0, 0.1);
-    border-radius: 4px;
-    text-align: center;
-    &:hover {
-      cursor: pointer;
-      background-color: #d6d6d6;
-    }
-  }
-`;
-
 const ActionGroup = styled.div`
   display: flex;
   width: 100%;
@@ -193,15 +121,115 @@ const ActionGroup = styled.div`
   }
 `;
 
-const Badge = styled.div`
-  background-color: ${myColors.green300};
-  font-weight: bold;
-  color: ${tailwindColors.white};
-  padding: 2px;
+const ArchiveDataTable = styled.table`
+  width: 100%;
+  table-layout: fixed;
+  border-collapse: collapse;
   text-align: center;
+  font-size: 14px;
+
+  col {
+    :nth-of-type(1) {
+      width: 4.5rem;
+    }
+    :nth-of-type(2) {
+      width: 15%;
+    }
+    :nth-of-type(3) {
+      width: 40%;
+    }
+    :nth-of-type(4) {
+      width: 20%;
+    }
+    :nth-of-type(5) {
+      width: 6rem;
+    }
+    :nth-of-type(6) {
+      width: 4.5rem;
+    }
+    :nth-of-type(7) {
+      width: 6rem;
+    }
+  }
+
+  thead {
+    border-bottom: 1px solid ${tailwindColors["grey-400"]};
+    background-color: ${tailwindColors["grey-100"]};
+    font-weight: bold;
+    text-align: center;
+    white-space: nowrap;
+
+    th {
+      padding: 0.5rem;
+    }
+  }
+  & tbody {
+    width: 100%;
+
+    tr {
+      width: 100%;
+      height: 2.5rem;
+      border-bottom: 1px solid ${tailwindColors["grey-400"]};
+      background-color: ${tailwindColors.white};
+
+      td {
+        padding: 0.3rem;
+
+        &.country-list {
+        }
+        &.origin-title {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+
+          a:hover {
+            color: ${myColors.blue500};
+            text-decoration: underline;
+          }
+        }
+        &.publisher {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+        &.publish-date {
+        }
+        &.page {
+          min-width: 4.5rem;
+        }
+        &.curation-request {
+          & > * {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+            width: max-content;
+            margin: 0 auto;
+            padding: 0.2rem 0.5rem;
+            font-weight: bold;
+            white-space: nowrap;
+          }
+
+          & > button {
+            border: solid 1px ${tailwindColors["grey-400"]};
+            &:hover {
+              cursor: pointer;
+              background-color: ${tailwindColors["grey-200"]};
+            }
+          }
+
+          & > div {
+            background-color: ${myColors.green300};
+            color: ${tailwindColors.white};
+          }
+        }
+      }
+    }
+  }
 `;
 
 const PaginationWrap = styled.div`
   margin-top: 1.5rem;
 `;
+
 export default UserArchiveDataList;
